815bb03f32052bc6622545a45c60f3603093df6f
Angelina Gupta <124087673+angelina10504@users.noreply.github.com>
2024-01-21 20:17:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ document.querySelector("#show-login").addEventListener("click",function(){
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
 });
+
+function onClickMenu() {
+    var menuContainer = document.getElementById("menu-container");
+    
+    menuContainer.classList.toggle("menu-open");
+}
