17c3634459cc5caff686b846e10387a9cc320f6d
Angelina Gupta <124087673+angelina10504@users.noreply.github.com>
2024-01-21 20:30:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+function onClickMenu() {
+    var menuContainer = document.getElementById("menu-container");
+    
+    menuContainer.classList.toggle("menu-open");
+}
+
